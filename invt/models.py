424f1484c8a3c648d33880bd2434aa4e4f4fb7a0
ioriveur(iori-yja) <fivo.11235813@gmail.com>
2011-10-24 22:00:50 +0900

    passed with japanese comment refs #4

--- invt/models.py
+++ invt/models.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from django.db import models
 
 HurryLevel = (
@@ -7,17 +8,14 @@ HurryLevel = (
 	('C','Normal'),
 	('N','Whenever'),
 )
-
 class Needs(models.Model):
 	pub_date = models.DateTimeField('date published')
 	pay_by_school = models.BooleanField() #hoge
 	price = models.CharField(max_length=20)
 	shop = models.CharField(max_length=20)
 	name = models.CharField(max_length=20)
-	properties = models.CharField(max_length=140)
-	adress = models.CharField(max_length=140)
-	level = models.CharField(max_length=1,choices=HurryLevel)
-	url = models.CharField(max_length=200)
-	def __unicode__(self):
-		return self.name
-
+	properties = models.CharField(max_length=140)#型番や属性(抵抗値とか)
+#	-種別
+	adress = models.CharField(max_length=140)#登録者のメールアドレス
+#	-至急度別の数
+	url = models.CharField(max_length=200)#注意書きや解説記事へのリファレンス
