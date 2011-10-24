73b3729197c9b43ef7f4ee14e39dc9c03198415f
ioriveur(iori-yja) <fivo.11235813@gmail.com>
2011-10-24 22:10:04 +0900

    refs #4, another kind of ports

--- invt/models.py
+++ invt/models.py
@@ -1,16 +1,9 @@
 # -*- coding: utf-8 -*-
 from django.db import models
 
-HurryLevel = (
-	('M','immediately'),
-	('A','At once'),
-	('B','Hurry'),
-	('C','Normal'),
-	('N','Whenever'),
-)
 class Needs(models.Model):
 	pub_date = models.DateTimeField('date published')
-	pay_by_school = models.BooleanField() #hoge
+	pay_by_school = models.BooleanField()#部費か?
 	price = models.CharField(max_length=20)
 	shop = models.CharField(max_length=20)
 	name = models.CharField(max_length=20)
@@ -18,4 +11,9 @@ class Needs(models.Model):
 #	-種別
 	adress = models.CharField(max_length=140)#登録者のメールアドレス
 #	-至急度別の数
+	Mnum = models.IntegerField('How we need it tomorrow')
+	Inum = models.IntegerField('How we need it immediately')
+	Hnum = models.IntegerField('Hurry')
+	Nnum = models.IntegerField('Normal')
+	Wnum = models.IntegerField('whenever')
 	url = models.CharField(max_length=200)#注意書きや解説記事へのリファレンス
