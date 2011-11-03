60a852ef3def77aec2b754f5b1e71ec7bcf5a717
ioriveur(iori-yja) <fivo.11235813@gmail.com>
2011-11-04 00:43:45 +0900

    views

--- jukai/invt/views.py
+++ jukai/invt/views.py
@@ -1,7 +1,11 @@
 from jukai.invt.models import Part
+from django.template import Context, loader
 from django.http import HttpResponse
 
 def index(request):
-	latest_part_list = Part.objects.all().order_by('up_date')[:5]
-	output = ', '.join([p.name for p in latest_part_list])
-	return HttpResponse(output)
+	latest_part = Part.objects.all().order_by('up_date')[:5]
+	t = loader.get_template('html/hoge.html')
+	c = Context({
+		'latest_part': latest_part,
+	})
+	return HttpResponse(t.render(c))
